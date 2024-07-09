704a91f96697f64ae57071649c98de7e3f8ace77
aryan1403 <aaryan14032006@gmail.com>
2024-07-09 09:41:08 +0530

    login register

--- index.js
+++ index.js
@@ -1,37 +1,33 @@
 const express = require('express') // Importing the express module
-const {getTodoById, getAllTodo} = require('./db')
 const app = express() // const... creating the app
 const Todo = require('./todo')
 const port = process.env.PORT || 3000;
+const bodyparser = require('body-parser');
+const db = require('./db/db');
+app.use(bodyparser.json());
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-// TODO --> task manager
+// Login
+app.post("/login", async (req, res) => {
+    const body = req.body;
+    const email = body.email;
+    const pass = body.pass;
 
-// Get All Todos
-app.get('/all', (req, res) => {
-    const todos = getAllTodo();
-    res.json({
-        data: todos,
-        msg: "Success"
-    })
+    const result = await db.find({"email": email, "pass": pass});
+    if(result.length == 1) {
+        res.json({msg: "Login successfull", status: 200})
+    } else {
+        res.json({msg: "Login Failed", status: 400})
+    }
 })
 
-// Get a Todo
-app.get('/todo/:id', (req, res) => {
-    const id = req.params.id;
-    const todo = getTodoById(id);
+// register
+app.post("/register", async (req, res) => {
+    const body = req.body;
 
-    res.json({
-        data: todo,
-        msg: "Success"
-    })
+    const result = await db.create(body);
+    res.status(201).json({msg: "user registered successfully"})
 })
-
-
-// Create a TODO
-
-
-// Delete a TODO
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
